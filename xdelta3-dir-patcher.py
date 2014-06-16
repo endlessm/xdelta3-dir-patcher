1f77ace0a2988aab387c339ec9768864be716138
Srdjan Grubor <sgnn7@sgnn7.org>
2014-06-16 14:10:54 -0500

    Initial commit of xdelta diff script

---
+++ xdelta3-dir-patcher.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class XDelta3DirPatcher(object):
+    def __init__(self):
+        print("Initializing patcher")
+
+    def run(self):
+        print("Running delta3...")
+
+if __name__ == '__main__':
+    patcher = XDelta3DirPatcher()
+    patcher.run()
