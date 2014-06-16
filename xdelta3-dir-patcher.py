8a0c60ac77b5bc31786c3d315e97d15d013696e7
Srdjan Grubor <sgnn7@sgnn7.org>
2014-06-16 15:06:48 -0500

    Added basic framework around doing the xdelta generation
      Added creation of the xdelta3, parameter parsing, and copying of modes from
      the original files.

--- xdelta3-dir-patcher.py
+++ xdelta3-dir-patcher.py
@@ -1,12 +1,94 @@
 #!/usr/bin/env python3
 
+import argparse
+from os import chmod, makedirs, path, sep, stat, walk
+from subprocess import call
+from stat import *
+
+
+VERSION='0.1'
+
+# Allows for invoking attributes as methods/functions
+class AttributeDict(dict):
+    def __getattr__(self, attr):
+        return self[attr]
+    def __setattr__(self, attr, value):
+        self[attr] = value
+
 class XDelta3DirPatcher(object):
-    def __init__(self):
+    def __init__(self, args):
         print("Initializing patcher")
+
+        self.args = args
+
+    def _find_file_delta(self, rel_path, new_file, old_root, new_root, target_root):
+        print("\nProcessing %s" % new_file)
+
+        target_path = path.join(target_root, rel_path)
+
+        if not path.exists(target_path):
+            makedirs(target_path)
+
+        old_path = path.join(old_root, rel_path, new_file)
+        new_path = path.join(new_root, rel_path, new_file)
+        target_path = path.join(target_path, new_file)
+
+        if args.debug:
+            print([old_path, new_path, target_path])
+
+        if not path.isfile(old_path):
+            old_path = None
+            print("Old file not present. Ignoring source in XDelta")
+
+        command = ['xdelta3', '-f', '-e']
+        if old_path:
+            command.append('-s')
+            command.append(old_path)
+
+        command.append(new_path)
+        command.append(target_path)
+
+        print("Generating xdelta: %s" % command)
+        call(command)
+
+        print("Copying mode data")
+        new_mode = S_IMODE(stat(new_path).st_mode)
+        chmod(target_path, new_mode)
+
+        if args.debug:
+            print("Mode: %s" % new_mode)
 
     def run(self):
         print("Running delta3...")
 
+        for root, dirs, new_files in walk(args.new_dir):
+            rel_path = path.relpath(root, args.new_dir).split(sep)[0]
+
+            print('-'*10, root, '-'*10)
+            print(new_files)
+            for new_file in new_files:
+                self._find_file_delta(rel_path, new_file, args.old_dir, args.new_dir, args.target_dir)
+
 if __name__ == '__main__':
-    patcher = XDelta3DirPatcher()
-    patcher.run()
+    parser = argparse.ArgumentParser(description='Creates and applies XDelta3-based directory diff tgz files')
+
+    parser.add_argument('old_dir', \
+            help='Folder containing the old version of the files')
+
+    parser.add_argument('new_dir', \
+            help='Folder containing the new version of the files')
+
+    parser.add_argument('target_dir', \
+            help='Destination folder for the generated diff')
+
+    parser.add_argument('--debug', \
+            help='Enable debugging output', \
+            action='store_true')
+
+    parser.add_argument('--version', \
+            action='version', \
+            version='%(prog)s v' + VERSION)
+
+    args = AttributeDict(vars(parser.parse_args()))
+
+    XDelta3DirPatcher(args).run()
