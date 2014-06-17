5659d627b57a54304580e3f4028dce6967500794
Srdjan Grubor <sgnn7@sgnn7.org>
2014-06-17 11:45:12 -0500

    Added a version test of the dir patcher

--- tests/test_xdelta3_dir_patcher.py
+++ tests/test_xdelta3_dir_patcher.py
@@ -1,5 +1,14 @@
 import unittest
+from subprocess import check_output
+
+# Dashes are standard for exec scipts but not allowed for modules in Python. We
+# use the script standard since we will be running that file as a script most
+# often.
+__import__("xdelta3-dir-patcher")
 
 class TestXdelta3DirPatcher(unittest.TestCase):
-    def test_foo(self):
-        self.assertEqual(True, True)
+    EXECUTABLE="xdelta3-dir-patcher.py"
+
+    def test_version_is_correct(self):
+        output = check_output(["./%s" % self.EXECUTABLE, '--version'])
+        self.assertEqual(output, "%s v0.1\n" % self.EXECUTABLE)
