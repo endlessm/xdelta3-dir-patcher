c38f9f0ddd220bc7b509cc4fc0d55f959e96ef6d
Srdjan Grubor <sgnn7@sgnn7.org>
2015-03-24 11:57:05 -0500

    Added ability to add files to the dir listing object
    Now we can add file objects to the directory listings. Explicit params
    were forced to add_File to ensure that the resulting object will have
    all the needed properties set.

--- tests/test_dir_listing.py
+++ tests/test_dir_listing.py
@@ -26,6 +26,10 @@ from mock import Mock
 # often.
 patcher = imp.load_source("xdelta3-dir-patcher", "xdelta3-dir-patcher")
 
+class FakeMember(object):
+    def __init__(self, version):
+        self.version = version
+
 class TestDirListing(unittest.TestCase):
     def setUp(self):
         self.test_class = patcher.DirListing
@@ -33,8 +37,43 @@ class TestDirListing(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_dir_listing_starts_with_no_directories_nor_files(self):
+    # Helpers
+    def add_mock_file(self, test_object, version):
+        data = FakeMember(version)
+        test_object.add_file("name%s" % version,
+                             data,
+                             1 + version,
+                             "username%s" % version,
+                             10 + version,
+                             "groupname%s" % version,
+                             100 + version)
+
+    def verify_mock_files(self, test_object, versions):
+        self.assertEqual(len(versions), len(test_object.files))
+
+        for version in versions:
+            file_obj = test_object.files[version - 1]
+
+            self.assertEquals(file_obj.name, "name%s" % version)
+            self.assertEquals(file_obj.permissions, 1 + version)
+            self.assertEquals(file_obj.uname, "username%s" % version)
+            self.assertEquals(file_obj.uid, 10 + version)
+            self.assertEquals(file_obj.gname, "groupname%s" % version)
+            self.assertEquals(file_obj.gid, 100 + version)
+            self.assertEquals(file_obj.data.version, version)
+
+    # Tests
+
+    def test_starts_with_no_directories_nor_files(self):
         test_object = self.test_class()
 
         self.assertEqual([], test_object.dirs)
         self.assertEqual([], test_object.files)
+
+    def test_adding_files_works(self):
+        test_object = self.test_class()
+
+        self.add_mock_file(test_object, 1)
+        self.add_mock_file(test_object, 2)
+
+        self.verify_mock_files(test_object, [1, 2])
